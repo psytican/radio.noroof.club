c4678bd358fa82c8268b0d7cc44d9126d443e521
Evgeny Panov <evgeny.panov@xgroup.cy>
2024-10-11 12:35:00 +0300

    1

--- docker/images/ezstream/bin/playlist.sh
+++ docker/images/ezstream/bin/playlist.sh
@@ -4,6 +4,8 @@ current_path="$( cd "$( dirname "$0" )" && pwd )"
 
 playlist_path=$current_path/playlist.m3u
 
+previous_playlist_path=/tmp/icecast2_previous_playlist
+previous_playlist_path_temp=/tmp/icecast2_previous_playlist_temp
 previous_track_path=/tmp/icecast2_previous_track
 current_track_path=/tmp/icecast2_current_track
 next_track_path=/tmp/icecast2_next_track
@@ -14,13 +16,19 @@ next_track_path=/tmp/icecast2_next_track
 
 find -L $current_path/playlist/ -type f -name "*.mp3" -exec echo {} \; > $playlist_path
 
+playlist_count=$(wc -l < $playlist_path)
+previous_playlist_count=$($playlist_count * 0.50)
+tail -n $previous_playlist_count $previous_playlist_path > $previous_playlist_path_temp && mv $previous_playlist_path_temp $previous_playlist_path
+
 get_random_track () {
     random_track=$(shuf -n 1 $playlist_path)
 
     track_prev=$(cat $previous_track_path)
     track_now=$(cat $current_track_path)
 
-    if [ "$random_track" == "$track_prev" ] || [ "$random_track" == "$track_now" ]
+    in_previous_playlist_path=$(grep -q "$random_track" $previous_playlist_path)
+
+    if [ "$random_track" == "$track_prev" ] || [ "$random_track" == "$track_now" || $in_previous_playlist_path ]
     then
         random_track=$(get_random_track)
     fi
@@ -41,5 +49,7 @@ else
 fi
 
 echo "$(get_random_track)" > $next_track_path
+
+cat $current_track_path >> previous_playlist_path
 
 cat $current_track_path
