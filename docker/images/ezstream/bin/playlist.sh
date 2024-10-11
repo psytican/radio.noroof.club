fd904ecf56ba0599bff4f51c9cb935c52a6a18af
Evgeny Panov <evgeny.panov@xgroup.cy>
2024-10-11 13:23:00 +0300

    1

--- docker/images/ezstream/bin/playlist.sh
+++ docker/images/ezstream/bin/playlist.sh
@@ -6,12 +6,14 @@ playlist_path=$current_path/playlist.m3u
 
 previous_playlist_path=/tmp/icecast2_previous_playlist
 previous_playlist_path_temp=/tmp/icecast2_previous_playlist_temp
+
+[ -f previous_playlist_path ] || touch previous_playlist_path
+[ -f previous_playlist_path_temp ] || touch previous_playlist_path_temp
+
 previous_track_path=/tmp/icecast2_previous_track
 current_track_path=/tmp/icecast2_current_track
 next_track_path=/tmp/icecast2_next_track
 
-[ -f previous_playlist_path ] || touch previous_playlist_path
-[ -f previous_playlist_path_temp ] || touch previous_playlist_path_temp
 [ -f $previous_track_path ] || touch $previous_track_path
 [ -f $current_track_path ] || touch $current_track_path
 [ -f $next_track_path ] || touch $next_track_path
@@ -19,8 +21,13 @@ next_track_path=/tmp/icecast2_next_track
 find -L $current_path/playlist/ -type f -name "*.mp3" -exec echo {} \; > $playlist_path
 
 playlist_count=$(wc -l < $playlist_path)
-previous_playlist_count=$($playlist_count * 0.50)
-tail -n $previous_playlist_count $previous_playlist_path > $previous_playlist_path_temp && mv $previous_playlist_path_temp $previous_playlist_path
+previous_playlist_count=$(wc -l < $previous_playlist_path)
+previous_playlist_tail_count=$(($playlist_count / 2))
+
+if [ "$previous_playlist_count" -lt "$previous_playlist_tail_count" ]
+then
+    tail -n $previous_playlist_tail_count $previous_playlist_path > $previous_playlist_path_temp && mv $previous_playlist_path_temp $previous_playlist_path
+fi
 
 get_random_track () {
     random_track=$(shuf -n 1 $playlist_path)
