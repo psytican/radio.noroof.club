32f907892a4405ac47c4962a915fcb620594db53
Evgeny Panov <evgeny.panov@xgroup.cy>
2024-10-11 14:16:53 +0300

    1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # radio.noroof.club
 Stream Radio NOROOF.CLUB
+
+# Manual testing
+/var/ezstream/playlist.sh && cat /tmp/icecast2_previous_playlist
--- docker/images/ezstream/bin/playlist.sh
+++ docker/images/ezstream/bin/playlist.sh
@@ -24,7 +24,7 @@ playlist_count=$(wc -l < $playlist_path)
 previous_playlist_count=$(wc -l < $previous_playlist_path)
 previous_playlist_tail_count=$(($playlist_count / 2))
 
-if [ "$previous_playlist_count" -lt "$previous_playlist_tail_count" ]
+if [ "$previous_playlist_tail_count" -lt "$previous_playlist_count" ]
 then
     tail -n $previous_playlist_tail_count $previous_playlist_path > $previous_playlist_path_temp && mv $previous_playlist_path_temp $previous_playlist_path
 fi
@@ -35,9 +35,9 @@ get_random_track () {
     track_prev=$(cat $previous_track_path)
     track_now=$(cat $current_track_path)
 
-    in_previous_playlist_path=$(grep -q "$random_track" $previous_playlist_path)
+    in_previous_playlist_path=$(grep "$random_track" $previous_playlist_path)
 
-    if [ "$random_track" == "$track_prev" ] || [ "$random_track" == "$track_now" ] || [ $in_previous_playlist_path ]
+    if [ "$random_track" == "$track_prev" ] || [ "$random_track" == "$track_now" ] || [ -s "$in_previous_playlist_path" ]
     then
         random_track=$(get_random_track)
     fi
@@ -57,8 +57,8 @@ else
     echo "$(get_random_track)" > $current_track_path
 fi
 
-echo "$(get_random_track)" > $next_track_path
+cat $current_track_path >> $previous_playlist_path
 
-cat $current_track_path >> previous_playlist_path
+echo "$(get_random_track)" > $next_track_path
 
 cat $current_track_path
